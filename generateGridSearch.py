969f3f75a9f1bdac781090ac1d798856dee7e70c
ooakley <49024315+ooakley@users.noreply.github.com>
2023-11-27 16:36:39 +0000

    updates to gridsearch

--- generateGridSearch.py
+++ generateGridSearch.py
@@ -3,17 +3,21 @@ import numpy as np
 # Defines the parameter ranges, with [start, stop, num_steps]:
 GRIDSEARCH = {
     "superIterationCount": [10, 10, 1],
-    "numberOfCells": [150, 150, 1],
+    "numberOfCells": [200, 200, 1],
     "worldSize": [1024, 1024, 1],
     "gridSize": [32, 32, 1],
+    "cellTypeProportions": [0, 0, 1],
+    "matrixPersistence": [0.99, 0.99, 1],
     "wbK": [1, 1, 1],
     "kappa": [3, 3, 1],
     "homotypicInhibition": [0.2, 0.8, 4],
     "heterotypicInhibition": [0, 0, 1],
-    "polarityPersistence": [0.1, 0.9, 4],
-    "polarityTurningCoupling": [0.1, 0.9, 4],
-    "flowScaling": [1, 4, 4],
-    "flowPolarityCoupling": [0.8, 2, 4],
+    "polarityPersistence": [0.4, 0.6, 5],
+    "polarityTurningCoupling": [0.7, 0.9, 5],
+    "flowScaling": [2.5, 3.5, 5],
+    "flowPolarityCoupling": [0.4, 8, 5],
+    "collisionRepolarisation": [0, 0, 1],
+    "repolarisationRate": [1, 1, 1]
 }
 
 def main():
@@ -21,13 +25,17 @@ def main():
     argument_grid = [
         (
             int(superIterationCount), int(numberOfCells), int(worldSize), int(gridSize),
+            cellTypeProportions, matrixPersistence,
             wbK, kappa, homotypicInhibition, heterotypicInhibition, polarityPersistence,
-            polarityTurningCoupling, flowScaling, flowPolarityCoupling 
+            polarityTurningCoupling, flowScaling, flowPolarityCoupling,
+            collisionRepolarisation, repolarisationRate
         ) \
         for superIterationCount in np.linspace(*GRIDSEARCH["superIterationCount"]) \
         for numberOfCells in np.linspace(*GRIDSEARCH["numberOfCells"]) \
         for worldSize in np.linspace(*GRIDSEARCH["worldSize"]) \
         for gridSize in np.linspace(*GRIDSEARCH["gridSize"]) \
+        for cellTypeProportions in np.linspace(*GRIDSEARCH["cellTypeProportions"]) \
+        for matrixPersistence in np.linspace(*GRIDSEARCH["matrixPersistence"]) \
         for wbK in np.linspace(*GRIDSEARCH["wbK"]) \
         for kappa in np.linspace(*GRIDSEARCH["kappa"]) \
         for homotypicInhibition in np.linspace(*GRIDSEARCH["homotypicInhibition"]) \
@@ -35,7 +43,9 @@ def main():
         for polarityPersistence in np.linspace(*GRIDSEARCH["polarityPersistence"]) \
         for polarityTurningCoupling in np.linspace(*GRIDSEARCH["polarityTurningCoupling"]) \
         for flowScaling in np.linspace(*GRIDSEARCH["flowScaling"]) \
-        for flowPolarityCoupling in np.linspace(*GRIDSEARCH["flowPolarityCoupling"])
+        for flowPolarityCoupling in np.linspace(*GRIDSEARCH["flowPolarityCoupling"]) \
+        for collisionRepolarisation in np.linspace(*GRIDSEARCH["collisionRepolarisation"]) \
+        for repolarisationRate in np.linspace(*GRIDSEARCH["repolarisationRate"])
     ]
 
     print(len(argument_grid))
--- gridsearch.sh
+++ gridsearch.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=cpu
 #SBATCH --time=6:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --array=1-1024
+#SBATCH --array=1-2500
 
 # Required lmod modules:
 # Boost/1.81.0-GCC-12.2.0
@@ -18,17 +18,25 @@ superIterationCount=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print 
 numberOfCells=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $3}' $config)
 worldSize=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $4}' $config)
 gridSize=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $5}' $config)
-wbK=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $6}' $config)
-kappa=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $7}' $config)
-homotypicInhibition=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $8}' $config)
-heterotypicInhibition=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $9}' $config)
-polarityPersistence=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $10}' $config)
-polarityTurningCoupling=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $11}' $config)
-flowScaling=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $12}' $config)
-flowPolarityCoupling=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $13}' $config)
+cellTypeProportions=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $6}' $config)
+matrixPersistence=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $7}' $config)
+
+wbK=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $8}' $config)
+kappa=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $9}' $config)
+homotypicInhibition=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $10}' $config)
+heterotypicInhibition=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $11}' $config)
+polarityPersistence=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $12}' $config)
+polarityTurningCoupling=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $13}' $config)
+flowScaling=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $14}' $config)
+flowPolarityCoupling=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $15}' $config)
+collisionRepolarisation=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $16}' $config)
+repolarisationRate=$(awk -v array_id=$SLURM_ARRAY_TASK_ID '$1==array_id {print $17}' $config)
 
 # Running simulation with specified parameters:
 ./build/src/main --jobArrayID $SLURM_ARRAY_TASK_ID --superIterationCount $superIterationCount --numberOfCells $numberOfCells \
-    --worldSize $worldSize --gridSize $gridSize --wbK $wbK --kappa $kappa --homotypicInhibition $homotypicInhibition \
+    --worldSize $worldSize --gridSize $gridSize \
+    --cellTypeProportions $cellTypeProportions --matrixPersistence $matrixPersistence \
+    --wbK $wbK --kappa $kappa --homotypicInhibition $homotypicInhibition \
     --heterotypicInhibition $heterotypicInhibition --polarityPersistence $polarityPersistence --polarityTurningCoupling $polarityTurningCoupling \
-    --flowScaling $flowScaling --flowPolarityCoupling $flowPolarityCoupling
+    --flowScaling $flowScaling --flowPolarityCoupling $flowPolarityCoupling \
+    --collisionRepolarisation $collisionRepolarisation --repolarisationRate $repolarisationRate
