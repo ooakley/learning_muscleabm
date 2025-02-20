27ef05e35c32b7dd1dbc21ac5b5cfd26aeae5a5b
ooakley <49024315+ooakley@users.noreply.github.com>
2025-02-20 14:58:28 +0000

    changes to sweep

--- python_scripts/generateSobolSamples.py
+++ python_scripts/generateSobolSamples.py
@@ -49,7 +49,7 @@ INTEGER_PARAMETERS = [
 CONSTANT_PARAMETERS = {
     "superIterationCount": 10,
     "timestepsToRun": 576,
-    "numberOfCells": 200,
+    "numberOfCells": 150,
     "worldSize": 2048,
     "gridSize": 64,
     "thereIsMatrixInteraction": 1,
@@ -93,8 +93,9 @@ def generate_LHS_matrix(rng):
     parameter_matrix = []
     for parameter_index in range(len(GRIDSEARCH_PARAMETERS)):
         # Generate columns of parameter matrix:
-        # parameter_array = np.linspace(0, 1, NUM_SAMPLES)
-        parameter_array = rng.uniform(size=(NUM_SAMPLES))
+        # parameter_array = rng.uniform(size=(NUM_SAMPLES))
+        parameter_array = np.linspace(0, 1, NUM_SAMPLES)
+        rng.shuffle(parameter_array)
         parameter_matrix.append(parameter_array)
     # Stack along columns:
     parameter_matrix = np.stack(parameter_matrix, axis=1)
