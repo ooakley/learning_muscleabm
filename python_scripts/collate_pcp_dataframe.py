ac0ab013389fbbc98d81431ccd1eb74e399d46ee
ooakley <49024315+ooakley@users.noreply.github.com>
2025-05-19 14:03:17 +0100

    altering specifics of collecting op data

--- python_scripts/collate_pcp_dataframe.py
+++ python_scripts/collate_pcp_dataframe.py
@@ -2,29 +2,30 @@ import json
 import numpy as np
 
 
-GRIDSEARCH_PARAMETERS = [
-    "matrixAdditionRate",
-    "matrixTurnoverRate",
-    "flowScaling",
-    "cellBodyRadius",
-    "eccentricity",
-    "polarityDiffusionRate",
-    "actinAdvectionRate",
-    "contactAdvectionRate",
-    "inhibitionStrength",
-    "maxCellAngularConcentration",
-    "maxMatrixAngularConcentration"
-]
+GRIDSEARCH_PARAMETERS = {
+    "matrixAdditionRate": [0.05, 0.25],
+    "matrixTurnoverRate": [0.05, 0.25],
+    "maxCellAngularConcentration": [1, 5],
+    "maxMeanActinFlow": [1, 5],
+    "flowScaling": [0, 0.2],
+    "polarityDiffusionRate": [0, 0.05],
+    "actinAdvectionRate": [0., 2.],
+    "contactAdvectionRate": [0., 2.],
+    "maxMatrixAngularConcentration": [1, 5],
+    "cellBodyRadius": [60, 200],
+    "eccentricity": [0, 0.98],
+    "inhibitionStrength": [0.0, 1.0]
+}
 
 
 def main():
     input_matrix = []
-    for i in range(13000):
+    for i in range(150000):
         with open(f'./fileOutputs/{i}/{i}_arguments.json') as json_data:
             # Reading data:
             parameter_dict = json.load(json_data)
             input_row = []
-            for parameter_name in GRIDSEARCH_PARAMETERS:
+            for parameter_name in GRIDSEARCH_PARAMETERS.keys():
                 input_row.append(parameter_dict[parameter_name])
             input_matrix.append(np.array(input_row))
     input_matrix = np.stack(input_matrix, axis=0)
